ab89771bbcc54ab941c0c49c0b2b7ed00698b87b
Sulkhi <sulkhi25@gmail.co>
2022-06-19 20:12:21 +0700

    react intro

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import style from "./contact.module.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className={style.card}>
+      <img className={style.card__image} src={data.photo} alt={data.name} />
+      <div className={style.card__content}>
+        <h5 className={style.card__phone}>{data.phone}</h5>
+        <div className={style.card__body}>
+          <h2 className={style.card__title}>{data.name}</h2>
+          <p className={style.card__email}>{data.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import style from "./header.module.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <header className={style.header__container}>
+      <div className={style.header__content}>
+        <h1 className={style.header__title}>Call a Friend</h1>
+        <p className={style.header__desc}>your friendly contact app</p>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
